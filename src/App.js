1d941578a088201e2f43747a31d764eef3981c93
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2023-09-01 18:30:06 +0100

    Stripe submenus complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Hero from './Hero'
-import Sidebar from './Sidebar'
-import Submenu from './Submenu'
-
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
       <Sidebar />
+      <Hero />
       <Submenu />
     </>
-  )
+  );
 }
 
-export default App
-
-// <></>
+export default App;
--- src/context.js
+++ src/context.js
@@ -1,51 +1,48 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
-
-// AppContext() -> Parent -> method 
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
 const AppContext = React.createContext();
 
-// AppProvider() Function with its children props (States and functions)
-export const AppProvider = ({children}) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
 
-    // OpenSidebar() Function
-    const OpenSidebar = () => {
-        setIsSidebarOpen(true);
-    }
-    // CloseSidebar() Function
-    const CloseSidebar = () => {
-        setIsSidebarOpen(false);
-    }
-    // OpenSubmenu() Function
-    const OpenSubmenu = () => {
-        setIsSubmenuOpen(true);
-    }
-    // CloseSubmenu() Function
-    const CloseSubmenu = () => {
-        setIsSubmenuOpen(false);
-    }
-
-    return (
-        <AppContext.Provider
-            value={{
-                isSubmenuOpen,
-                isSidebarOpen,
-                OpenSubmenu,
-                OpenSidebar,
-                CloseSubmenu,
-                CloseSidebar
-
-            }}
-        >
-            {children}
-        </AppContext.Provider>
-    )
-}
-
-// Custom Hook
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
 export const useGlobalContext = () => {
-    return useContext(AppContext);
-}
+  return useContext(AppContext);
+};
 
-// <></>
+export { AppContext, AppProvider };
